d8d536659d9ff4789c816e004b000d644c1c94e4
김동현 <emboob@naver.com>
2019-11-22 20:49:21 +0900

    실습문제 10번 구현 완료. 11번 쉘 만들기 남음.

--- use_shm_sem_copy.c
+++ use_shm_sem_copy.c
@@ -1,222 +1,159 @@
-/**
+﻿/**
  * 시스템 프로그래밍 lab3 과제 실습문제 10번
  * 파일 이름 : use_shm_sem_copy.c
  * 만든이 : 20153265 김동현
  */
-#include <sys/types.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <semaphore.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <sys/types.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h>
+#include <sys/sem.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <string.h>
-#include <fcntl.h>
-#include <errno.h>
+#include <unistd.h>
 
-#define SEMNAME "/my_sem"
 #define SHMSIZE 1024
-#define SHMNAME "/my_shm"
+#define SHMKEY (key_t)0111
+#define SEMKEY (key_t)0111
 
-void psem_parent(sem_t *semd);
-void psem_child(sem_t *semd);
-void p(sem_t *semd);
-void v(sem_t *semd);
+static int  semid;
 
-int main(int argc, char *argv[])
+union semapore
 {
-    int shmd, len;
-    int i, val;
-    int src_fd, dst_fd;
+    int value;
+    struct semid_ds *buf;
+    unsigned short int *array;
+};
+
+static int semid;
+
+// 부모 프로세스 (파일 읽기 역할만 수행)
+int parent_process_read(char *filename)
+{
+	int shmid, len;
+	void *shmaddr;
+	FILE *fp = fopen(filename, "r");
+	int size = strlen((char *)shmaddr);
+	
+	printf("부모 프로세스(읽기) 시작!\n");
+	sleep(3);
+
+	if ((shmid = shmget(SHMKEY, SHMSIZE, IPC_CREAT|0666)) == -1) { 
+		perror("shmget failed"); 
+		exit(1); 
+	} 
+
+	if ((shmaddr = shmat(shmid, NULL, 0)) == (void *)-1) { 
+		perror("shmat failed"); 
+		exit(1); 
+	} 
+
+	fgets((char *)shmaddr, size, fp);
+
+	if (shmdt(shmaddr) == -1) { 
+		perror("shmdt failed"); 
+		exit(1);
+	}
+
+	printf("Read Success\n");
+	fclose(fp);
+	printf("부모 프로세스 종료.\n\n");
+}
+
+// 자식 프로세스 (파일 쓰기 역할만 수행)
+int child_process_write(char *filename)
+{
+	int shmid, len; 
+	void *shmaddr;
+	FILE *fp = fopen(filename, "w");
+
+	printf("자식 프로세스(쓰기) 시작!\n");
+	sleep(2);
+
+	if ((shmid = shmget(SHMKEY, SHMSIZE,IPC_CREAT|0666)) == -1)
+	{ 
+		perror ("shmget failed"); 
+		exit(1); 
+	}
+
+	if ((shmaddr=shmat(shmid, NULL, 0)) == (void *)-1)
+	{ 
+		perror ("shmat failed"); 
+		exit(1); 
+	}
+
+	fputs((char *)shmaddr, fp);
+	printf("Copy Success!\n");
+
+	if (shmdt(shmaddr) == -1)
+	{ 
+		perror ("shmdt failed"); 
+		exit (1); 
+	}
+
+	if (shmctl(shmid, IPC_RMID, 0) == -1)
+	{ 
+		perror ("shmctl failed"); 
+		exit (1); 
+	}
+
+	fclose(fp);
+	printf("자식 프로세스 종료.\n\n");
+}
+
+int main(int argc, char **argv)
+{
+	FILE* fp;
+	union semapore semapore;
+
+	struct sembuf mysem_open  = {0, -1, SEM_UNDO};
+    struct sembuf mysem_close = {0, 1, SEM_UNDO};
+
+	if (argc != 3)
+	{
+        fprintf(stderr, "사용 방법 : %s <orignial file name> <create file name>\n", argv[0]);
+        exit(1);
+	}
+
+	semid = semget((key_t)234, 1, 0660|IPC_CREAT);
     
-    char ch[SHMSIZE];
-    
-    sem_t *semd;
-    pid_t pid;
-    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-    ssize_t read_count;
-
-    void *shmaddr;
-    
-    if((semd = sem_open(SEMNAME, O_CREAT, 0600, 1)) == SEM_FAILED) {
-        perror("sem_open failed");
-        exit(1);
+	if (semid == -1)
+	{
+        perror("semget error");
+        exit(0);
     }
 
-    sem_getvalue(semd, &val);
+	semapore.value = 1;
 
-    if(argc != 3)
-    {
-        fprintf(stderr, "사용 방법 : ./%s <원본 파일명> <복사할 파일명>\n", argv[0]);
-        exit(1);
+    if (semctl(semid, 0, SETVAL, semapore) == -1)
+	{   
+        perror("semctl error");
+		exit(1); 
     }
 
-    pid = fork();
-
-    // fork 실패
-    if(pid == -1)
-    {
-        perror("can't fork\n");
-        exit(1);
+	if(semop(semid, &mysem_open, 1) == -1)
+	{
+        perror("semop error");
+        exit(0);
     }
 
-    // 자식 프로세스
-    if(pid == 0)
-    {
-        p(semd);
-        printf("자식 프로세스 시작\n");
-
-        sleep(3);
-
-        if((shmaddr = mmap(0, SHMSIZE, PROT_WRITE, MAP_SHARED, shmd, 0)) == MAP_FAILED)
-        {
-            perror("mmap_failed");
-            exit(1);
-        }
-
-        if(ftruncate(shmd, SHMSIZE) != 0)
-        {
-            perror("ftruncate failed");
-            exit(1);
-        }
-
-        if(mlock(shmaddr, SHMSIZE) != 0)
-        {
-            perror("mlock failed");
-            exit(1);
-        }
-
-        // 파일복사 시작
-        if((dst_fd = creat(argv[2], mode)) == -1)
-        {
-            perror("dst open");
-            exit(1);
-        }
-
-        while((read_count = read(src_fd, ch, SHMSIZE)) > 0)
-        {
-            write(dst_fd, shmaddr, read_count);
-        }
-
-        if (read_count < 0)
-        {
-            perror("read");
-        }
-
-        close(dst_fd);
-        // 끝
-
-        //strcpy((char *)shmaddr, read(dst_fd, ch, read_count));
-
-        if(munmap(shmaddr, SHMSIZE) == -1)
-        {
-            perror("munmap failed");
-            exit(1);
-        }
-        close(shmd);
-
-        printf("자식 프로세스 종료\n");
-        v(semd);
-    }
-    // 부모 프로세스
-    else
-    {
-        p(semd);
-        printf("부모 프로세스 시작\n");
-
-        sleep(3);
-
-        if((shmaddr = mmap(0, SHMSIZE, PROT_READ, MAP_SHARED, shmd, 0)) == MAP_FAILED)
-        {
-            perror("mmap_failed");
-            exit(1);
-        }
-
-        if(mlock(shmaddr, SHMSIZE) != 0)
-        {
-            perror("mlock failed");
-            exit(1);
-        }
-
-        if((src_fd = open(argv[1], O_RDONLY)) == -1)
-        {
-            perror("src_open");
-            exit(1);
-        }
-
-        strcpy((char *)shmaddr, (char *)read(dst_fd, ch, read_count));
-
-        close(src_fd);
-
-        printf("%s", (char*)shmaddr);
-
-        if(munmap(shmaddr, SHMSIZE) == -1)
-        {
-            perror("munmap failed");
-            exit(1);
-        }
-
-        close(shmd);
-
-        if (shm_unlink(SHMNAME) == -1)
-        {
-            perror ("shm_unlink failed");
-            exit(1);
-        }
-
-        printf("부모 프로세스 종료\n");
-        v(semd);
+    if ((fp = fopen(argv[1], "r+")) == NULL)
+	{
+        perror("fopen error");
+        exit(0);
     }
 
-    if(sem_close(semd) == -1) {
-        perror("sem_close failed");
-        exit(1);
-    }
-    
-    if(sem_unlink(SEMNAME) == -1) {
-        perror("sem_unlink failed");
-        exit(1);
-    }
+	parent_process_read(argv[1]);
+	rewind(fp);
+
+	sleep(2);
+
+	child_process_write(argv[2]);
+	fclose(fp);
+
+	semop(semid, &mysem_close, 1);
+
+	printf("만들어진 %s 파일을 확인하세요.\n", argv[2]);
 
     return 0;
-}
-
-void psem_parent(sem_t *semd)
-{
-    p(semd);
-    printf("process %d : semaphore in use\n", getpid());
-    
-    printf("process %d : putting semaphore\n", getpid());
-    v(semd);
-    exit(0);
-}
-
-void psem_child(sem_t *semd)
-{
-    p(semd);
-    printf("process %d : semaphore in use\n", getpid());
-    sleep(rand()%5);
-    printf("process %d : putting semaphore\n", getpid());
-    v(semd);
-    exit(0);
-}
-
-void p(sem_t *semd)
-{
-    int ret;
-    if((ret = sem_trywait(semd)) != 0 && errno == EAGAIN)
-        sem_wait(semd);
-    else if (ret != 0)
-    {
-        perror("sem_trywait failed");
-        exit(1);
-    }
-}
-
-void v(sem_t *semd)
-{
-    if(sem_post(semd) != 0) {
-        perror("sem_post failed");
-        exit(1);
-    }
 }
